5bbbf31e272a3c1a3eb0a708a6a8237193d4dce8
Feranmi <feranmia285@gmail.com>
2026-06-21 06:24:22 -0700

    Update CORS for production

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3002", "http://localhost:3001", "http://localhost:3003", "http://localhost:3005", "https://ai-data-analysis-backend.onrender.com"],
+    allow_origins=["http://localhost:3000", "http://localhost:3002", "http://localhost:3001", "http://localhost:3003", "http://localhost:3005", "https://fedanb12-ai-data-analysis-tool.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
